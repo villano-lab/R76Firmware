3675bb2def8394e3d6c85b77ff6957b9124f5969
Kitt <kathryn.harris@ucdenver.edu>
2022-04-06 12:48:44 -0600

    Copy @villaa 's work on Na22 firmware into repository.

---
+++ Na22Trigger/library/C/lib/src/RegisterFile.h
@@ -0,0 +1,6 @@
+#define SCI_REG_thrs 0x00000000
+#define SCI_REG_polarity 0x00000001
+#define SCI_REG_inhib 0x00000002
+#define SCI_REG_ratereset 0x00020000
+#define SCI_REG_RateMeter_0_FIFOADDRESS 0x10000
+
