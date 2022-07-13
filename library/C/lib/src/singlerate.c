61264bc579379c2883c5191e3b525aae60750267
Kitty <kathryn.harris@ucdenver.edu>
2022-07-13 13:40:29 -0600

    Change from currentrate to singlerate with optional register setting

--- library/C/lib/src/singlerate.c
+++ library/C/lib/src/singlerate.c
@@ -27,18 +27,16 @@
 const char* program_name = "scanrate";
 
 void print_usage(FILE* stream, int exit_code){ //This looks unaligned but lines up correctly in the terminal output
-	fprintf (stream, "Usage:  %s options \n", program_name);
-  	fprintf (stream, DET_TEXT);
-	fprintf (stream, DELAY_TEXT);
-	fprintf (stream, INHIB_TEXT);
-	fprintf (stream, TOP_TEXT);
-	fprintf (stream, THRESH_TEXT);
-	fprintf (stream, SKIP_TEXT);
+	fprintf (stream, "Usage:  %s [options] -- [options to pass to setregisters] \n", program_name);
+  	fprintf (stream, CONFIG_TEXT);
 	fprintf (stream, VERBOSE_TEXT);
 	fprintf (stream, SILENT_TEXT);
 	fprintf (stream, LOG_TEXT);
 	fprintf (stream, VERSION_TEXT);
 	fprintf (stream, HELP_TEXT);
+	subhelp(stream);
+
+	exit (exit_code);
 
 	exit (exit_code);
 };
@@ -47,7 +45,7 @@ int main(int argc, char* argv[])
 {
 	//Read options
 	while(iarg != -1){
-		iarg = getopt_long(argc, argv, "+d:i:l::shv::Vg:D:T:t:S:", longopts, &ind);
+		iarg = getopt_long(argc, argv, "l::c::shv::V+", longopts, &ind);
 		switch (iarg){
 		case 'h':
 			print_usage(stdout,0);
@@ -79,32 +77,28 @@ int main(int argc, char* argv[])
 				logfile = fopen("log.txt","w");
 			};
 			break;
-		case 'D':
-			selection = optarg;
-			parse_detector_switch(selection);
-			if(value < 0 ){return -1;} //If there's an error, pass it through.
-			break;
-		case 'g':
-			if(verbose > 2){printf("Hey I'm in case g\n");}
-			gateflag = 1;
-			gtemp = optarg;
-			break;
-		case 't':
-			thrs = atoi(optarg);
-			break;
-		case 'i':
-			inhib = atoi(optarg);
-			break;
-		case 'd':
-			delay = atoi(optarg);
-			break;
-		case 'T':
-			top = atoi(optarg);
-			break;
-		case 'S':
-			skip = atoi(optarg);
+		case 'c':
+			if(optarg){
+				configfilename = optarg;
+			}else{
+				configfilename = "example.config";
+			}
+			read_config(configfilename);
+			char* command = malloc(100);
+			snprintf(command,100,"./setregisters -c %s -v%d",configfilename,verbose);
+			system(command);
+			free(command);
 			break;
 		}
+	}
+
+	if(optind!=argc){ //if there are args to pass through, tell the user,
+		if(verbose > 0){printf("Running setregisters utility.\n");}
+		//then construct, run, and free the command.
+		char* command = malloc(100);
+		snprintf(command,100,"./setregisters %s -v%d",argv[optind],verbose);
+		system(command);
+		free(command);
 	}
 
 	//Verbosity message
@@ -130,52 +124,6 @@ int main(int argc, char* argv[])
 		return connect_q;
 	}
 
-	//Now set all the values we determined above
-	disable_q = disable_dets(disable_t, disable);
-
-	for(int i=0; i<24; i++){
-		if(disable_q[i] != 0){
-			printf("Unable to set on/off state of detector #%d! Aborting.\n",i);
-			return -1;
-		}
-	}
-
-	//Final run setup	
-	if(logfile != NULL){
-		fprintf(logfile,"============ Settings ============\n");
-		fprintf(logfile,"Starting threshold:			%d\n",thrs);
-		fprintf(logfile,"Trigger Inhibition Time:		%d\n",inhib);
-		fprintf(logfile,"Upper Gate:					%d\n",gate_u);
-		fprintf(logfile,"Lower Gate: 					%d\n",gate_l);
-		fprintf(logfile,"Polarity (Neg 0, Pos 1):		%d\n",polarity);
-		fprintf(logfile,"Lower threshold scanning from %d to %d in steps of %d.\n",range_l,range_u,range_s);
-		fprintf(logfile,"Detectors enabled:				\n");
-		for(int i=0;i++;i<24){
-			if(disable[i] == 0){fprintf(logfile,"%d, ",i);}
-		}
-		fprintf(logfile,"\b\b\n\n"); //clear trailing comma and space before inserting two newlines.
-	};
-	
-	//Pass them along to the system
-	if(verbose>0){printf("Configuring...\n");};
-	thrs_q = set_by_polarity(REG_thrsh_SET,polarity,thrs);
-	if(thrs_q != 0){
-		printf("Error from REG_thrsh_SET. Aborting.\n");
-		return thrs_q;
-	}
-	top_q = set_by_polarity(REG_top_SET,polarity,top);
-	if(top_q != 0){
-		printf("Error from REG_top_SET. Aborting.\n");
-		return top_q;
-	}
-	inhib_q = REG_inhib_SET(inhib,&handle);			//Set number of samples to delay data by
-	delay_q = REG_delay_SET(delay,&handle);			//Set number of samples to delay data by
-	gate_uq = REG_gate_u_SET(gate_u,&handle);			
-	gate_lq = REG_gate_l_SET(gate_l,&handle);
-	polarity_q = REG_polarity_SET(polarity,&handle);	//Set polarity to negative
-	skip_q = REG_skip_SET(skip,&handle);
-	
-	//Run phase - undo reset
 	if(verbose>0){printf("Setting up rate counter... \n");};
 	tic = time(NULL);
     	
